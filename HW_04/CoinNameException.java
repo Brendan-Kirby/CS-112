f45d887b80fcd089504b4a96f3f70ffdea09501b
Brendan-Kirby <37196444+Brendan-Kirby@users.noreply.github.com>
2018-03-16 11:49:26 -0700

    comment deletion problem - copypaste from ide

--- HW_04/CoinNameException.java
+++ HW_04/CoinNameException.java
@@ -1,21 +1,44 @@
+/* CoinNameException.java - Defines an exception class to be thrown when an 
+ * 							invalid coin name is entered.
+ * 
+ * Author:  Brendan Kirby
+ * Module:  04
+ * Project: 1
+ * 
+ * Description
+ * 
+ * 		Constants
+ * 			DEFAULT_MESSAGE (String) - contains the default message for this 
+ * 									   exception object when thrown.
+ * 		Instance Variables
+ * 			None
+ * 		Methods
+ * 			default constructor - calls super with DEFAULT_MESSAGE as its 
+ * 								  sole parameter.
+ * 			full constructor - takes message as a parameter, calls super with
+ * 							   the argument given.
+ */  
+
 public class CoinNameException extends Exception {
 
-	private final String DEFAULT_MESSAGE = "\n\nInvalid coin name, try again. Valid coin names are : \n" +
-										   "* Penny\n" +
-										   "* Nickel\n" +
-										   "* Dime\n" +
-										   "* Quarter\n" +
-										   "* Half-Dollar\n\n";
+	//default constant
+	private static final String DEFAULT_MESSAGE = "\n\nInvalid coin name, try again. Valid coin names are : \n" +
+												  "* Penny\n" +
+												  "* Nickel\n" +
+												  "* Dime\n" +
+												  "* Quarter\n" +
+												  "* Half-Dollar\n\n";
 	
+	//default constructor
 	public CoinNameException() {
 	
-		System.out.println(DEFAULT_MESSAGE);
+		super(DEFAULT_MESSAGE);
 			
 	}
 	
+	//full constructor
 	public CoinNameException(String nameError) {
 		
 		super(nameError);
 	}
-
 }
