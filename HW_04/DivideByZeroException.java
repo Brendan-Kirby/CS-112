96d3c07b1f1be31c62ccd0f5878a3fbeceeddf6a
Brendan-Kirby <37196444+Brendan-Kirby@users.noreply.github.com>
2018-03-16 11:56:11 -0700

    comment deletion problem - copypaste from ide

--- HW_04/DivideByZeroException.java
+++ HW_04/DivideByZeroException.java
@@ -1,10 +1,32 @@
-public class DivideByZeroException extends Exception {
+/* DivideByZeroException.java - Defines an exception class to be thrown when a 
+ * 								denominator of zero is entered in a division
+ * 								expression.
+ * 
+ * Author:  Brendan Kirby
+ * Module:  04
+ * Project: 2
+ * 
+ * Description
+ * 
+ * 		Constants
+ * 			DEFAULT_MESSAGE (String) - contains the default message for this 
+ * 									   exception object when thrown.
+ * 		Instance Variables
+ * 			None
+ * 		Methods
+ * 			default constructor - calls super with DEFAULT_MESSAGE as its 
+ * 								  sole parameter.
+ * 			full constructor - takes message as a parameter, calls super with
+ * 							   the argument given.
+ */  
 
-	private final String DEFAULT_MESSAGE = "Error - Attempted division by zero"; 
+public class DivideByZeroException extends Exception { 
+
+	private static final String DEFAULT_MESSAGE = "Error - Attempted division by zero";
 
 	public DivideByZeroException() {
 	
-		System.out.println(DEFAULT_MESSAGE);
+		super(DEFAULT_MESSAGE);
 	}
 	
 	public DivideByZeroException(String message) {
