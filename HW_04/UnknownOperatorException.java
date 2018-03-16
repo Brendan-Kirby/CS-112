22df0a38e9ee6b561dfe40174ead3d458cea7f37
Brendan-Kirby <37196444+Brendan-Kirby@users.noreply.github.com>
2018-03-16 11:56:43 -0700

    comment deletion problem - copypaste from ide

--- HW_04/UnknownOperatorException.java
+++ HW_04/UnknownOperatorException.java
@@ -1,10 +1,31 @@
-public class UnknownOperatorException extends Exception {
+/* UnknownOperatorException.java - Defines an exception class to be thrown when an 
+ * 								   invalid operator is entered.
+ * 
+ * Author:  Brendan Kirby
+ * Module:  04
+ * Project: 2
+ * 
+ * Description
+ * 
+ * 		Constants
+ * 			DEFAULT_MESSAGE (String) - contains the default message for this 
+ * 									   exception object when thrown.
+ * 		Instance Variables
+ * 			None
+ * 		Methods
+ * 			default constructor - calls super with DEFAULT_MESSAGE as its 
+ * 								  sole parameter.
+ * 			full constructor - takes message as a parameter, calls super with
+ * 							   the argument given.
+ */  
 
-	private final String DEFAULT_MESSAGE = "Error - Unknown Operator";
+public class UnknownOperatorException extends Exception {
+	
+	private static final String DEFAULT_MESSAGE = "Error - Unknown Operator"; 
 
 	public UnknownOperatorException() {
 	
-		System.out.println(DEFAULT_MESSAGE);
+		super(DEFAULT_MESSAGE);
 	}
 	
 	public UnknownOperatorException(String message) {
