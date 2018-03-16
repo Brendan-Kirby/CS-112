12b1cedcb837c5ad15264e9a6557d81af8717845
Brendan-Kirby <37196444+Brendan-Kirby@users.noreply.github.com>
2018-03-16 11:48:34 -0700

    comment deletion problem - copypaste from ide

--- HW_04/CoinValueException.java
+++ HW_04/CoinValueException.java
@@ -1,13 +1,37 @@
+/* CoinValueException.java - Defines an exception class to be thrown when an 
+ * 			    			 invalid coin value is entered.
+ * 
+ * Author:  Brendan Kirby
+ * Module:  04
+ * Project: 1
+ * 
+ * Description
+ * 
+ * 		Constants
+ * 			DEFAULT_MESSAGE (String) - contains the default message for this 
+ * 									   exception object when thrown.
+ * 		Instance Variables
+ * 			None
+ * 		Methods
+ * 			default constructor - calls super with DEFAULT_MESSAGE as its 
+ * 								  sole parameter.
+ * 			full constructor - takes message as a parameter, calls super with
+ * 							   the argument given.
+ */   
+
 public class CoinValueException extends Exception {
 
-	String defaultValueError = "\n\nCoin identity did not match coin value, try again.\n\n";
+	//default constant
+	private static String DEFAULT_MESSAGE = "\n\nCoin identity did not match coin value, try again.\n\n";
 
+	//default constructor
 	public CoinValueException() {
 		
-		System.out.println(defaultValueError);
+		super(DEFAULT_MESSAGE);
 		
 	}
-
+	
+	//full constructor
 	public CoinValueException(String valueError) {
 	
 		super(valueError);	
